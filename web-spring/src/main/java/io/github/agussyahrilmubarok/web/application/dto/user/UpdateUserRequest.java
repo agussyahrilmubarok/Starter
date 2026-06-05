6cbd9cf5052b19e8a901f01b0ce3558172afd1f0
mrschwartx <mrschwartx@gmail.com>
2026-06-05 10:01:37 +0700

    fix(dev-web-spring): Convention name

--- web-spring/src/main/java/io/github/agussyahrilmubarok/web/application/dto/user/UpdateUserRequest.java
+++ web-spring/src/main/java/io/github/agussyahrilmubarok/web/application/dto/user/UpdateUserRequest.java
@@ -6,7 +6,7 @@ import java.util.UUID;
 
 public record UpdateUserRequest(
         UUID id,
-        
+
         String name,
 
         @Email(message = "Email is not valid")
--- web-spring/src/test/java/io/github/agussyahrilmubarok/web/application/service/impl/UserServiceImplTest.java
+++ web-spring/src/test/java/io/github/agussyahrilmubarok/web/application/service/impl/UserServiceImplTest.java
@@ -26,9 +26,7 @@ import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
